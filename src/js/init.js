b52c7ecad28645c71b654a7a7f3a0449a23edf42
Kasheftin <Kasheftin@gmail.com>
2012-11-08 23:05:45 +0200

    ko, ko.map, jquery, jquery-ui libs updated to new versions

--- src/js/init.js
+++ src/js/init.js
@@ -1,9 +1,11 @@
 require.config({
 	baseUrl: 'js',
 	paths: {
-		'jquery': 'wild-libs/jquery',
-		'jquery-ui': 'wild-libs/jquery-ui',
+		'jquery': 'wild-libs/jquery-1.8.2.min',
+		'jquery-ui': 'wild-libs/jquery-ui-1.9.1.min',
 		'setImmediate': 'wild-libs/setImmediate',
+		'knockout': 'knockout-2.2.0.min',
+		'knockout.mapping': 'knockout.mapping-2.3.3.min'
 	},
 	shim: {
     	'jquery': { exports: function(){ return jQuery.noConflict(true); } },
