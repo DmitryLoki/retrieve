52f06752912eba34346a445dc0e0ee01349d19d5
Kasheftin <Kasheftin@gmail.com>
2013-04-28 12:37:26 +0300

    simple page init config updated with cors

--- src/js/initSimple.js
+++ src/js/initSimple.js
@@ -1,5 +1,5 @@
 require.config({
-	baseUrl: 'js',
+	baseUrl: 'http://airvis03.ragneta.com/js',
 	paths: {
 		'jquery': 'wild-libs/jquery',
 		'setImmediate': 'wild-libs/setImmediate',
@@ -7,7 +7,14 @@ require.config({
 	shim: {
     	'jquery': { exports: function(){ return jQuery.noConflict(true); } },
     	'setImmediate': { exports: 'window.setImmediate' },
-	}
+	},
+    config: {
+        text: {
+            useXhr: function (url, protocol, hostname, port) {
+                return true;
+            }
+        }
+    }
 });
 
 require(['core', 'domReady!', 'widget!AppSimple'], function(core, doc, App) {
