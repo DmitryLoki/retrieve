f6b6bc51b3c611a16ee647b294fae5892a629c12
Vyacheslav Malchik <info@vkey.biz>
2013-07-09 17:27:27 +0000

    Default contestId and raceId has been removed.

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
 
+error_reporting(0);
+
 $tmpl = join("",file("index.html"));
-$tmpl = str_replace("<!-- contestId -->",$_GET["contestId"] ? $_GET["contestId"] : "cnts-130530-264712753",$tmpl);
-$tmpl = str_replace("<!-- raceId -->",$_GET["raceId"] ? $_GET["raceId"] : "r-d3215c15-ce48-4914-b658-b5cd46ac2f98",$tmpl);
+$tmpl = str_replace("<!-- contestId -->",$_GET["contestId"] ? $_GET["contestId"] : "",$tmpl);
+$tmpl = str_replace("<!-- raceId -->",$_GET["raceId"] ? $_GET["raceId"] : "",$tmpl);
 $tmpl = str_replace("<!-- apiVersion -->",$_GET["apiVersion"] ? $_GET["apiVersion"] : "v0.2",$tmpl);
 
 echo $tmpl;
