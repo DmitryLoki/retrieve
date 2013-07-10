e59dff6bf27dba586d9433c1fe219fea87aee104
Vyacheslav Malchik <info@vkey.biz>
2013-07-10 11:45:34 +0000

    GET arguments renamed to contestid and raceid.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 error_reporting(0);
 
 $tmpl = join("",file("index.html"));
-$tmpl = str_replace("<!-- contestId -->",$_GET["contestId"] ? $_GET["contestId"] : "",$tmpl);
-$tmpl = str_replace("<!-- raceId -->",$_GET["raceId"] ? $_GET["raceId"] : "",$tmpl);
+$tmpl = str_replace("<!-- contestId -->",$_GET["contestid"], $tmpl);
+$tmpl = str_replace("<!-- raceId -->",$_GET["raceid"], $tmpl);
 $tmpl = str_replace("<!-- apiVersion -->",$_GET["apiVersion"] ? $_GET["apiVersion"] : "v0.2",$tmpl);
 
 echo $tmpl;
