3848f73a651182c8aa9c60db3b1cea01290790d1
Svad <svadglo@gmail.com>
2013-09-17 16:24:39 +0400

    #3826

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 error_reporting(0);
 
 $tmpl = join("",file("index.html"));
-$tmpl = str_replace("<!-- contestId -->",$_GET["contestid"], $tmpl);
-$tmpl = str_replace("<!-- raceId -->",$_GET["raceid"], $tmpl);
-$tmpl = str_replace("<!-- apiVersion -->",$_GET["apiVersion"] ? $_GET["apiVersion"] : "v0.2",$tmpl);
+$tmpl = str_replace("!-- contestId --",$_GET["contestid"], $tmpl);
+$tmpl = str_replace("!-- raceId --",$_GET["raceid"], $tmpl);
+$tmpl = str_replace("!-- apiVersion --",$_GET["apiVersion"] ? $_GET["apiVersion"] : "v0.2",$tmpl);
 
 echo $tmpl;
